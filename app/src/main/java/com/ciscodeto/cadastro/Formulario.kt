6aebdcc64b131fb83189ec0ee0a94f3c5aea618b
ciscodeto <netofcisco@gmail.com>
2024-08-19 10:27:07 -0300

    initial commit

---
+++ app/src/main/java/com/ciscodeto/cadastro/Formulario.kt
@@ -0,0 +1,11 @@
+package com.ciscodeto.cadastro
+
+class Formulario {
+    val name: String = ""
+    val phoneNumber: String = ""
+    val email: String = ""
+    val isOnEmailList: Boolean = false
+    val sex: String = ""
+    val city: String = ""
+    val state: String = ""
+}
