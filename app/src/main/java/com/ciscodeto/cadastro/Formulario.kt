348731b7f2746e6d9d65b688c009052d8beeac8d
ciscodeto <netofcisco@gmail.com>
2024-08-19 13:31:11 -0300

    feat: clear button listener

--- app/src/main/java/com/ciscodeto/cadastro/Formulario.kt
+++ app/src/main/java/com/ciscodeto/cadastro/Formulario.kt
@@ -1,13 +1,14 @@
 package com.ciscodeto.cadastro
 
-class Formulario {
-    val name: String = ""
-    val phoneNumber: String = ""
-    val email: String = ""
-    val isOnEmailList: Boolean = false
-    val sex: String = ""
-    val city: String = ""
-    val state: String = ""
+class Formulario(
+    val name: String,
+    val phoneNumber: String,
+    val email: String,
+    val isOnEmailList: Boolean,
+    val sex: String,
+    val city: String,
+    val state: String
+) {
 
     override fun toString(): String {
         return "Nome: $name\n" +
--- app/src/main/java/com/ciscodeto/cadastro/MainActivity.kt
+++ app/src/main/java/com/ciscodeto/cadastro/MainActivity.kt
@@ -4,21 +4,23 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var name: EditText
-    private lateinit var phoneNumber: EditText
-    private lateinit var email: EditText
-    private lateinit var isOnEmailList: CheckBox
-    private lateinit var city: EditText
-    private lateinit var sex: RadioGroup
-    private lateinit var uf: Spinner
+    private lateinit var fullNameTxt: EditText
+    private lateinit var phoneNumberTxt: EditText
+    private lateinit var emailTxt: EditText
+    private lateinit var isOnEmailListCB: CheckBox
+    private lateinit var cityTxt: EditText
+    private lateinit var sexRG: RadioGroup
+    private lateinit var ufSpn: Spinner
     private lateinit var clearBtn: Button
     private lateinit var saveBtn: Button
 
@@ -26,7 +28,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val state: String = ""
+
+        fullNameTxt = findViewById(R.id.nameTxt)
+        phoneNumberTxt = findViewById(R.id.phoneTxt)
+        emailTxt = findViewById(R.id.emailTxt)
+        isOnEmailListCB = findViewById(R.id.emailListChk)
+        cityTxt = findViewById(R.id.cityTxt)
+        sexRG = findViewById(R.id.sexRG)
+        ufSpn = findViewById(R.id.planets_spinner)
+        clearBtn = findViewById(R.id.clearBtn)
+        saveBtn = findViewById(R.id.saveBtn)
+
+        clearBtn.setOnClickListener {
+            fullNameTxt.text.clear()
+            phoneNumberTxt.text.clear()
+            emailTxt.text.clear()
+            isOnEmailListCB.isChecked = false
+            cityTxt.text.clear()
+            sexRG.clearCheck()
+        }
+
+        saveBtn.setOnClickListener {
+
+        }
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
