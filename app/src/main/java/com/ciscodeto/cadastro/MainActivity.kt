a99159f96793fbda788eeaf94b1a8977e8c224ae
ciscodeto <netofcisco@gmail.com>
2024-08-19 12:02:43 -0300

    feat: Variables for input fields declared

--- app/src/main/java/com/ciscodeto/cadastro/MainActivity.kt
+++ app/src/main/java/com/ciscodeto/cadastro/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.ciscodeto.cadastro
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var name: EditText
+    private lateinit var phoneNumber: EditText
+    private lateinit var email: EditText
+    private lateinit var isOnEmailList: CheckBox
+    private lateinit var city: EditText
+    private lateinit var sex: RadioGroup
+    private lateinit var uf: Spinner
+    private lateinit var clearBtn: Button
+    private lateinit var saveBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val state: String = ""
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
